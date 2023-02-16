a9235e9c5f48ea22f69c2c8fbc92718b51b19b86
kevin monapara <kevinmonapara89@gmail.com>
2023-02-16 21:29:58 +0530

    commit

--- app/src/main/java/com/kevin/bmicalc/MainActivity.java
+++ app/src/main/java/com/kevin/bmicalc/MainActivity.java
@@ -2,6 +2,10 @@ package com.kevin.bmicalc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editTextWeight = findViewById(R.id.weight);
+        EditText editTextHeight = findViewById(R.id.height);
+        Button calculate = findViewById(R.id.calculate);
+        TextView textView = findViewById(R.id.result);
+
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float height = Float.parseFloat(String.valueOf(editTextHeight.getText())) / 100;
+                float weight = Float.parseFloat(String.valueOf(editTextWeight.getText()));
+                float bmi = weight / (height * height);
+                textView.setText(String.valueOf("BMI = "+bmi));
+            }
+        });
     }
 }
